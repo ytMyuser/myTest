a4da31278c036f2da1200283f11d6551bcaf5a2b
tarena <746604639@qq.com>
2020-07-27 20:08:08 +0800

    增加项目Model

--- myblog/myblog/settings/base.py
+++ myblog/myblog/settings/base.py
@@ -31,12 +31,17 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'blog',
+    'config',
+    'comment',
+
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
 ]
 
 MIDDLEWARE = [
--- myblog/myblog/settings/develop.py
+++ myblog/myblog/settings/develop.py
@@ -3,7 +3,11 @@ from . base import * #NOQA
 DEBUG=True
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'myblog',
+        'USER': 'root',
+        'PASSWORD': '123456',
+        'HOST':'127.0.0.1',
+        'PORT': '3306',
     }
 }
